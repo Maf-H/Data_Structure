3d415950c039a55a9a284f85e2ecfc63f7172d2b
Mesfin Haftu <mhtr03@gmail.com>
2024-06-17 10:48:49 +0300

    Update code and comment

--- quick_sort.py
+++ quick_sort.py
@@ -8,7 +8,7 @@ def quick_sort(input_list):
     splits the list into two(less than the pivot, and greater than the pivot) parts and sorts them recursively
     :param input_list: list to be sorted
     :return: sorted list
-    time complexity: O(nlogn) n times to compare and split, log(n) times to merge and sort
+    time complexity: best and average case O(n*log(n)), worst case O(n^2) n times to compare and split, log(n) times to merge and sort
     """
     if len(input_list) < 2:
         return input_list
@@ -17,9 +17,9 @@ def quick_sort(input_list):
         left = [x for x in input_list[1:] if x <= pivot]
         right = [x for x in input_list[1:] if x > pivot]
         return quick_sort(left) + [pivot] + quick_sort(right)
-t1 = perf_counter()  
 input_list = [randint(1, 1_000_000) for x in range(10_000_000)] # 10 million random numbers between 1 and 1,000,000
 # print(input_list)
+t1 = perf_counter()  
 quick_sort(input_list)
 print(perf_counter() - t1) # returns elapsed time in seconds(accurate) including sleep and system-wide
 print(process_time()) # returns elapsed time in seconds specific to current process.
